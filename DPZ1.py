6b65c0a6fdb6acdece5c54f9becb8ab2d42c9972
OlegSud <168107881+OlegSud@users.noreply.github.com>
2024-05-12 20:22:55 +0300

    Add files via upload

---
+++ DPZ1.py
@@ -0,0 +1,23 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+SB_Svod = {'Johnny': sum([5, 3, 3, 5, 4]) / len([5, 3, 3, 5, 4]), 'Bilbo': sum([2, 2, 2, 3]) / len([2, 2, 2, 3]), 'Steve': sum([4, 5, 5, 2]) / len([4, 5, 5, 2]), 'Khendrik': sum([4, 4, 3]) / len([4, 4, 3]), 'Aaron': sum([5, 5, 5, 4, 5]) / len([5, 5, 5, 4, 5])}
+print(SB_Svod)
+
+#Johnny = [5, 3, 3, 5, 4]
+#SB_Johnny = sum(Johnny) / len(Johnny)
+#Bilbo = [2, 2, 2, 3]
+#SB_Bilbo = sum(Bilbo) / len(Bilbo)
+#Steve = [4, 5, 5, 2]
+#SB_Steve = sum(Steve) / len(Steve)
+#Khendrik = [4, 4, 3]
+#SB_Khendrik = sum(Khendrik) / len(Khendrik)
+#Aaron = [5, 5, 5, 4, 5]
+#SB_Aaron = sum(Aaron) / len(Aaron)
+
+#SB_Svod = {'Johnny': SB_Johnny, 'Bilbo': SB_Bilbo, 'Steve': SB_Steve, 'Khendrik': SB_Khendrik, 'Aaron': SB_Aaron}
+
+#print(SB_Svod)
+
+
+
